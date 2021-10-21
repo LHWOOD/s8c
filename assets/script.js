6b4cae5355e7f3e573156dd96d9aad13cb292f45
LHWOOD <hutsonwood@gmail.com>
2021-10-21 15:13:06 -0600

    added nasa api

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,40 @@
-const yo = "yo";
+const room = document.querySelector("#space");
 
-console.log(yo);
+const button = document.querySelector("#btn");
+
+function blastOff() {
+  let requestUrl =
+    "https://api.nasa.gov/planetary/apod?api_key=1X7IFLdDZ3eBsOMix8bFmOge0yb0UZjWo3DgGbX1";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      button.setAttribute("style", "display: none");
+
+      let sPic = document.createElement("img");
+      let sTitle = document.createElement("h1");
+      let sDescription = document.createElement("p");
+
+      sPic.setAttribute(
+        "src",
+        "https://apod.nasa.gov/apod/image/2110/SH2-308NS_1024.jpg"
+      );
+      sTitle.textContent = data.title;
+      sDescription.textContent = data.explanation;
+
+      sPic.setAttribute("style", "width: 75%");
+      sTitle.setAttribute("style", "color: white");
+      sDescription.setAttribute("style", "color: white");
+
+      room.classList.add("room");
+      room.appendChild(sPic);
+      room.appendChild(sTitle);
+      room.appendChild(sDescription);
+    });
+}
+
+button.addEventListener("click", blastOff);
